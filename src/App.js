3e0f832761d8b0a8d70ee2acb3bc5cc0f0c917ff
Ankeet <ankeethanny1@gmail.com>
2020-04-04 20:16:54 +0530

    Google form added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { MdTrendingUp } from "react-icons/md";
+// import { MdTrendingUp } from "react-icons/md";
 
 function App() {
   const [data, setData] = useState([]);
@@ -18,7 +18,9 @@ function App() {
   return (
     <div className="mainContainer">
       <div>
-        <div className="header">CoronaVIRUS</div>
+        <div className="header">
+          <div className="header-message">Heard a Rumor? Get it confirmed <a href = "https://docs.google.com/forms/d/1JK4A2QRSA7Jz829_krw7V1Je43eSRjb8TCqw7CweABU/viewform?edit_requested=true" target="blank">here</a>.</div>
+          CoronaVIRUS</div>
         <div className="totalRecord">
           {Object.keys(total).length > 0 && <Total total={total} />}
         </div>
@@ -40,31 +42,31 @@ const Total = ({
       <div>
         <div className="listData">
           <span>Confirmed Cases: {confirmed}</span>
-          <span className="riseData">
+          {/* <span className="riseData">
             <MdTrendingUp />
             {delta.confirmed}
-          </span>
+          </span> */}
         </div>
         <div className="listData">
           <span>Active Cases: {active}</span>
-          <span className="riseData">
+          {/* <span className="riseData">
             <MdTrendingUp />
             {delta.active}
-          </span>
+          </span> */}
         </div>
         <div className="listData">
           <span>Deaths: {deaths}</span>
-          <span className="riseData">
+          {/* <span className="riseData">
             <MdTrendingUp />
             {delta.deaths}
-          </span>
+          </span> */}
         </div>
         <div className="listData">
           <span>Recovered: {recovered}</span>
-          <span className="riseData">
+          {/* <span className="riseData">
             <MdTrendingUp />
             {delta.recovered}
-          </span>
+          </span> */}
         </div>
       </div>
       <div className="updatedAt">Last updated at: {lastupdatedtime}</div>
@@ -86,6 +88,8 @@ class StateWise extends React.Component {
               <span>Confirmed: {s.confirmed}</span>
 
               <span>Active: {s.active}</span>
+
+              <span>Recovered: {s.recovered}</span>
 
               <span>Death: {s.deaths}</span>
             </div>
